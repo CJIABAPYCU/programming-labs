b230cf74ca3af5a395160fc56e8f39a74738ca81
Ivan_Pomidorov <snkr333@proton.me>
2026-05-02 22:27:20 +0300

    Add lab2 Pikachu.java

---
+++ lab2/src/Pokemons/Pikachu.java
@@ -0,0 +1,12 @@
+package Pokemons;
+
+import Attacks.*;
+import ru.ifmo.se.pokemon.*;
+
+public class Pikachu extends Pichu {
+    public Pikachu(String name, int level) {
+        super(name, level);
+        setStats(35, 55, 40, 50, 50, 90);
+        addMove(new Growl());
+    }
+}
